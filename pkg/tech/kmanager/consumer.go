7741c2b3cd584f273ffadd456df0e501c77d25b6
Cobalt0s <constantin.y.koval@gmail.com>
2022-10-17 18:38:30 +0300

    save

--- pkg/tech/kmanager/consumer.go
+++ pkg/tech/kmanager/consumer.go
@@ -2,6 +2,7 @@ package kmanager
 
 import (
 	"context"
+	"github.com/Cobalt0s/creme-brulee/pkg/rest/messaging"
 	"github.com/Cobalt0s/creme-brulee/pkg/stateful/config"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
@@ -16,21 +17,22 @@ type MessageConsumer struct {
 	consumer   *kafka.Consumer
 	topicNames []string
 	db         *gorm.DB
+	arguments  *messaging.ContextualArguments
 }
 
 type KafkaHealthChecker struct {
 	consumer *kafka.Consumer
 }
 
-func NewMessageConsumer(ctx context.Context, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string) *MessageConsumer {
-	return createMessageConsumer(ctx, db, cfg, clientID, consumerGroup, topicNames, false)
+func NewMessageConsumer(ctx context.Context, arguments *messaging.ContextualArguments, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string) *MessageConsumer {
+	return createMessageConsumer(ctx, arguments, db, cfg, clientID, consumerGroup, topicNames, false)
 }
 
-func NewMessageConsumerOrigin(ctx context.Context, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string) *MessageConsumer {
-	return createMessageConsumer(ctx, db, cfg, clientID, consumerGroup, topicNames, true)
+func NewMessageConsumerOrigin(ctx context.Context, arguments *messaging.ContextualArguments, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string) *MessageConsumer {
+	return createMessageConsumer(ctx, arguments, db, cfg, clientID, consumerGroup, topicNames, true)
 }
 
-func createMessageConsumer(ctx context.Context, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string, origin bool) *MessageConsumer {
+func createMessageConsumer(ctx context.Context, arguments *messaging.ContextualArguments, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string, origin bool) *MessageConsumer {
 	log := ctxlogrus.Extract(ctx)
 
 	kc, kafkaError := kafka.NewConsumer(getConsumerMap(origin, cfg, clientID, consumerGroup))
@@ -38,6 +40,7 @@ func createMessageConsumer(ctx context.Context, db *gorm.DB, cfg *config.KafkaCo
 		log.Fatal(kafkaError)
 	}
 	return &MessageConsumer{
+		arguments: arguments,
 		consumer:   kc,
 		topicNames: topicNames,
 		db:         db,
@@ -51,14 +54,14 @@ func getConsumerMap(origin bool, cfg *config.KafkaConfig, clientID, consumerGrou
 	return cfg.GetKafkaConfigMapConsumer(clientID, consumerGroup)
 }
 
-type TopicHandler func(ctx context.Context, db *gorm.DB, msg *kafka.Message) error
+type TopicHandler func(ctx context.Context, arguments *messaging.ContextualArguments,
+	db *gorm.DB, msg *kafka.Message) error
 
 func (mc *MessageConsumer) Start(ctx context.Context, handleMessage TopicHandler) error {
 	log := ctxlogrus.Extract(ctx)
 	log.Info("starting kafka consumer")
 
-	err := mc.consumer.SubscribeTopics(mc.topicNames, nil)
-	if err != nil {
+	if err := mc.consumer.SubscribeTopics(mc.topicNames, nil); err != nil {
 		log.Errorf("failed to subscirbe to kafka topics %v", err)
 		return err
 	}
@@ -68,7 +71,7 @@ func (mc *MessageConsumer) Start(ctx context.Context, handleMessage TopicHandler
 		// TODO how often do we ask kafka for messages?
 		if msg, err := mc.consumer.ReadMessage(-1); err == nil {
 			if len(msg.Key) != 0 { // ignore heartbeat messages
-				if err = handleMessage(ctx, mc.db, msg); err != nil {
+				if err = handleMessage(ctx, mc.arguments, mc.db, msg); err != nil {
 					log.Error("message will be NOT committed")
 					// TODO if we couldn't consume event from kafka we need to retry
 					// TODO do we fail the pod since it couldn't consume event?
@@ -76,7 +79,7 @@ func (mc *MessageConsumer) Start(ctx context.Context, handleMessage TopicHandler
 				}
 			}
 
-			if _, err := mc.consumer.CommitMessage(msg); err != nil {
+			if _, err = mc.consumer.CommitMessage(msg); err != nil {
 				log.Warnf("couldn't commit message %v", err)
 			}
 		} else {
