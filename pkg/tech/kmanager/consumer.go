38ac0b661f50926050993d394b7c3451334c547d
Cobalt0s <constantin.y.koval@gmail.com>
2023-01-03 21:29:54 +0200

    return Kafka Consumer cleanup func

--- pkg/tech/kmanager/consumer.go
+++ pkg/tech/kmanager/consumer.go
@@ -9,15 +9,13 @@ import (
 	"gorm.io/gorm"
 )
 
-const (
-	HealthTopic = "Healthz"
-)
-
 type MessageConsumer struct {
 	consumer   *kafka.Consumer
 	topicNames []string
 	db         *gorm.DB
 	arguments  *messaging.ContextualArguments
+	done       chan bool
+	poller     chan *kafka.Message
 }
 
 type KafkaHealthChecker struct {
@@ -32,6 +30,14 @@ func NewMessageConsumerOrigin(ctx context.Context, arguments *messaging.Contextu
 	return createMessageConsumer(ctx, arguments, db, cfg, clientID, consumerGroup, topicNames, true)
 }
 
+func (mc *MessageConsumer) Close() error {
+	select {
+	case mc.done <- true:
+	default:
+	}
+	return mc.consumer.Close()
+}
+
 func createMessageConsumer(ctx context.Context, arguments *messaging.ContextualArguments, db *gorm.DB, cfg *config.KafkaConfig, clientID, consumerGroup string, topicNames []string, origin bool) *MessageConsumer {
 	log := ctxlogrus.Extract(ctx)
 
@@ -40,10 +46,12 @@ func createMessageConsumer(ctx context.Context, arguments *messaging.ContextualA
 		log.Fatal(kafkaError)
 	}
 	return &MessageConsumer{
-		arguments: arguments,
+		arguments:  arguments,
 		consumer:   kc,
 		topicNames: topicNames,
 		db:         db,
+		done:       make(chan bool),
+		poller:     make(chan *kafka.Message),
 	}
 }
 
@@ -65,25 +73,32 @@ func (mc *MessageConsumer) Start(ctx context.Context, handleMessage TopicHandler
 		log.Errorf("failed to subscirbe to kafka topics %v", err)
 		return err
 	}
-	defer mc.consumer.Close()
 
-	for {
-		// TODO how often do we ask kafka for messages?
+	go func() {
+		// infinite Poller
 		if msg, err := mc.consumer.ReadMessage(-1); err == nil {
 			if len(msg.Key) != 0 { // ignore heartbeat messages
-				if err = handleMessage(ctx, mc.arguments, mc.db, msg); err != nil {
-					log.Error("message will be NOT committed")
-					// TODO if we couldn't consume event from kafka we need to retry
-					// TODO do we fail the pod since it couldn't consume event?
-					continue
-				}
-			}
-
-			if _, err = mc.consumer.CommitMessage(msg); err != nil {
-				log.Warnf("couldn't commit message %v", err)
+				mc.poller <-msg
 			}
 		} else {
 			log.Warnf("consumer kafka error: %v (%v)\n", err, msg)
+		}
+	}()
+
+	for {
+		select {
+		case <-mc.done:
+			return nil
+		case msg := <-mc.poller:
+			if err := handleMessage(ctx, mc.arguments, mc.db, msg); err != nil {
+				log.Error("message will be NOT committed")
+				// TODO if we couldn't consume event from kafka we need to retry
+				// TODO do we fail the pod since it couldn't consume event?
+				continue
+			}
+			if _, err := mc.consumer.CommitMessage(msg); err != nil {
+				log.Warnf("couldn't commit message %v", err)
+			}
 		}
 	}
 }
